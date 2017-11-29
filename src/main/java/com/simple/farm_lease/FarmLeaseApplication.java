ed9315af6cc1b62f64dc09895e24f4971356b7eb
geliang <geliang@geliang-PC>
2017-11-29 15:44:21 +0800

    init

---
+++ src/main/java/com/simple/farm_lease/FarmLeaseApplication.java
@@ -0,0 +1,12 @@
+package com.simple.farm_lease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmLeaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmLeaseApplication.class, args);
+	}
+}
