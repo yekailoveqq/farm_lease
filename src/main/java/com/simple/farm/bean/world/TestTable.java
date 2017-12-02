27d62b3425c13056519f836e1817dba10ac14beb
geliang <geliang@geliang-PC>
2017-12-02 11:00:10 +0800

    使用服务器上的mysql

---
+++ src/main/java/com/simple/farm/bean/world/TestTable.java
@@ -0,0 +1,21 @@
+package com.simple.farm.bean.world;
+/**
+ * 
+ * @author geliang
+ * 测试bean
+ */
+public class TestTable {
+
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+}
---
+++ src/main/java/com/simple/farm/dao/world/TestTableDao.java
@@ -0,0 +1,9 @@
+package com.simple.farm.dao.world;
+
+import com.simple.farm.bean.world.TestTable;
+
+import tk.mybatis.mapper.common.Mapper;
+
+public interface TestTableDao extends Mapper<TestTable>{
+
+}
