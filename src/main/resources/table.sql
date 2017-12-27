b7c4426cd5237ce41fafa3419abd9c9038678ef0
geliang <geliang@geliang-PC>
2017-12-27 17:13:49 +0800

    sql备份

--- src/main/resources/table.sql
+++ src/main/resources/table.sql
@@ -1,57 +1,116 @@
--- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
---
--- Host: 101.236.32.3    Database: farmdb
--- ------------------------------------------------------
--- Server version	5.6.38
+/*
+Navicat MySQL Data Transfer
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+Source Server         : yun
+Source Server Version : 50638
+Source Host           : 101.236.32.3:3306
+Source Database       : farmdb
 
---
--- Table structure for table `filed_info`
---
+Target Server Type    : MYSQL
+Target Server Version : 50638
+File Encoding         : 65001
 
+Date: 2017-12-27 17:12:51
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for `filed_info`
+-- ----------------------------
 DROP TABLE IF EXISTS `filed_info`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `filed_info` (
-  `id` int(11) NOT NULL,
-  `merchantid` varchar(45) DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `merchantid` int(45) DEFAULT NULL,
   `size` int(11) DEFAULT NULL,
   `monitorurl` varchar(45) DEFAULT NULL,
   `name` varchar(45) DEFAULT NULL,
   `state` varchar(45) DEFAULT NULL,
-  `version` varchar(45) DEFAULT NULL,
+  `version` int(45) DEFAULT NULL,
+  `price` float DEFAULT '30',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=InnoDB AUTO_INCREMENT=71 DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `filed_info`
---
+-- ----------------------------
+-- Records of filed_info
+-- ----------------------------
+INSERT INTO `filed_info` VALUES ('1', '1', '10', 'http://monitoe.com', 'd', '0', '1', '30');
+INSERT INTO `filed_info` VALUES ('2', '1', '10', 'http://monitoe.com', 'd', '0', '1', '30');
+INSERT INTO `filed_info` VALUES ('3', '1', '10', 'http://monitoe.com', 'd', '0', '1', '30');
+INSERT INTO `filed_info` VALUES ('4', '1', '10', 'http://monitoe.com', 'd', '2', '1', '30');
+INSERT INTO `filed_info` VALUES ('5', '1', '10', 'http://monitoe.com', 'd', '0', '1', '30');
+INSERT INTO `filed_info` VALUES ('6', '1', '10', 'http://monitoe.com', 'd', '1', '2', '30');
+INSERT INTO `filed_info` VALUES ('7', '1', '10', 'http://monitoe.com', 'd', '1', '1', '30');
+INSERT INTO `filed_info` VALUES ('8', '1', '10', 'http://monitoe.com', 'd', '1', '2', '30');
+INSERT INTO `filed_info` VALUES ('9', '1', '10', 'http://monitoe.com', 'd', '1', '2', '30');
+INSERT INTO `filed_info` VALUES ('10', '1', '10', 'http://monitoe.com', 'd', '1', '2', '30');
+INSERT INTO `filed_info` VALUES ('11', '1', '10', 'http://monitoe.com', 'd', '0', '1', '30');
+INSERT INTO `filed_info` VALUES ('12', '1', '10', 'http://monitoe.com', 'd', '2', '1', '30');
+INSERT INTO `filed_info` VALUES ('13', '1', '10', 'http://monitoe.com', 'd', '1', '2', '30');
+INSERT INTO `filed_info` VALUES ('14', '1', '10', 'http://monitoe.com', 'd', '2', '1', '30');
+INSERT INTO `filed_info` VALUES ('15', '1', '10', 'http://monitoe.com', 'd', '0', '1', '30');
+INSERT INTO `filed_info` VALUES ('16', '1', '10', 'http://monitoe.com', 'd', '0', '1', '30');
+INSERT INTO `filed_info` VALUES ('17', '1', '10', 'http://monitoe.com', 'd', '0', '1', '30');
+INSERT INTO `filed_info` VALUES ('18', '1', '10', 'http://monitoe.com', 'd', '0', '1', '30');
+INSERT INTO `filed_info` VALUES ('19', '1', '10', 'http://monitoe.com', 'd', '0', '1', '30');
+INSERT INTO `filed_info` VALUES ('20', '1', '10', 'http://monitoe.com', 'd', '0', '1', '30');
+INSERT INTO `filed_info` VALUES ('21', '1', '10', 'http://monitoe.com', 'd', '0', '1', '30');
+INSERT INTO `filed_info` VALUES ('22', '1', '10', 'http://monitoe.com', 'd', '0', '1', '30');
+INSERT INTO `filed_info` VALUES ('23', '1', '10', 'http://monitoe.com', 'd', '0', '1', '30');
+INSERT INTO `filed_info` VALUES ('24', '1', '10', 'http://monitoe.com', 'd', '0', '1', '30');
+INSERT INTO `filed_info` VALUES ('25', '1', '10', 'http://monitoe.com', 'd', '0', '1', '30');
+INSERT INTO `filed_info` VALUES ('26', '1', '10', 'http://monitoe.com', 'd', '0', '1', '30');
+INSERT INTO `filed_info` VALUES ('27', '1', '10', 'http://monitoe.com', 'd', '1', '2', '30');
+INSERT INTO `filed_info` VALUES ('28', '1', '10', 'http://monitoe.com', 'd', '0', '1', '30');
+INSERT INTO `filed_info` VALUES ('29', '1', '10', 'http://monitoe.com', 'd', '0', '1', '30');
+INSERT INTO `filed_info` VALUES ('30', '1', '10', 'http://monitoe.com', 'd', '0', '1', '30');
+INSERT INTO `filed_info` VALUES ('31', '1', '10', 'http://monitoe.com', 'd', '1', '2', '30');
+INSERT INTO `filed_info` VALUES ('32', '1', '10', 'http://monitoe.com', 'd', '0', '1', '30');
+INSERT INTO `filed_info` VALUES ('33', '1', '10', 'http://monitoe.com', 'd', '0', '1', '30');
+INSERT INTO `filed_info` VALUES ('34', '1', '10', 'http://monitoe.com', 'd', '0', '1', '30');
+INSERT INTO `filed_info` VALUES ('35', '1', '10', 'http://monitoe.com', 'd', '0', '1', '30');
+INSERT INTO `filed_info` VALUES ('36', '1', '10', 'http://monitoe.com', 'd', '0', '1', '30');
+INSERT INTO `filed_info` VALUES ('37', '1', '10', 'http://monitoe.com', 'd', '0', '1', '30');
+INSERT INTO `filed_info` VALUES ('38', '1', '10', 'http://monitoe.com', 'd', '0', '1', '30');
+INSERT INTO `filed_info` VALUES ('39', '1', '10', 'http://monitoe.com', 'd', '0', '1', '30');
+INSERT INTO `filed_info` VALUES ('40', '1', '10', 'http://monitoe.com', 'd', '0', '1', '30');
+INSERT INTO `filed_info` VALUES ('41', '1', '10', 'http://monitoe.com', 'd', '0', '1', '30');
+INSERT INTO `filed_info` VALUES ('42', '1', '10', 'http://monitoe.com', 'd', '0', '1', '30');
+INSERT INTO `filed_info` VALUES ('43', '1', '10', 'http://monitoe.com', 'd', '0', '1', '30');
+INSERT INTO `filed_info` VALUES ('44', '1', '10', 'http://monitoe.com', 'd', '0', '1', '30');
+INSERT INTO `filed_info` VALUES ('45', '1', '10', 'http://monitoe.com', 'd', '0', '1', '30');
+INSERT INTO `filed_info` VALUES ('46', '1', '10', 'http://monitoe.com', 'd', '0', '1', '30');
+INSERT INTO `filed_info` VALUES ('47', '1', '10', 'http://monitoe.com', 'd', '0', '1', '30');
+INSERT INTO `filed_info` VALUES ('48', '1', '10', 'http://monitoe.com', 'd', '0', '1', '30');
+INSERT INTO `filed_info` VALUES ('49', '1', '10', 'http://monitoe.com', 'd', '0', '1', '30');
+INSERT INTO `filed_info` VALUES ('50', '1', '10', 'http://monitoe.com', 'd', '1', '2', '30');
+INSERT INTO `filed_info` VALUES ('51', '1', '10', 'http://monitoe.com', 'd', '1', '2', '30');
+INSERT INTO `filed_info` VALUES ('52', '1', '10', 'http://monitoe.com', 'd', '1', '2', '30');
+INSERT INTO `filed_info` VALUES ('53', '1', '10', 'http://monitoe.com', 'd', '1', '2', '30');
+INSERT INTO `filed_info` VALUES ('54', '1', '10', 'http://monitoe.com', 'd', '0', '1', '30');
+INSERT INTO `filed_info` VALUES ('55', '1', '10', 'http://monitoe.com', 'd', '0', '1', '30');
+INSERT INTO `filed_info` VALUES ('56', '1', '10', 'http://monitoe.com', 'd', '0', '1', '30');
+INSERT INTO `filed_info` VALUES ('57', '1', '10', 'http://monitoe.com', 'd', '0', '1', '30');
+INSERT INTO `filed_info` VALUES ('58', '1', '10', 'http://monitoe.com', 'd', '0', '1', '30');
+INSERT INTO `filed_info` VALUES ('59', '1', '10', 'http://monitoe.com', 'd', '0', '1', '30');
+INSERT INTO `filed_info` VALUES ('60', '1', '10', 'http://monitoe.com', 'd', '0', '1', '30');
+INSERT INTO `filed_info` VALUES ('61', '1', '10', 'http://monitoe.com', 'd', '0', '1', '30');
+INSERT INTO `filed_info` VALUES ('62', '1', '10', 'http://monitoe.com', 'd', '0', '1', '30');
+INSERT INTO `filed_info` VALUES ('63', '1', '10', 'http://monitoe.com', 'd', '0', '1', '30');
+INSERT INTO `filed_info` VALUES ('64', '1', '10', 'http://monitoe.com', 'd', '0', '1', '30');
+INSERT INTO `filed_info` VALUES ('65', '1', '10', 'http://monitoe.com', 'd', '0', '1', '30');
+INSERT INTO `filed_info` VALUES ('66', '1', '10', 'http://monitoe.com', 'd', '0', '1', '30');
+INSERT INTO `filed_info` VALUES ('67', '1', '10', 'http://monitoe.com', 'd', '0', '1', '30');
+INSERT INTO `filed_info` VALUES ('68', '1', '10', 'http://monitoe.com', 'd', '0', '1', '30');
+INSERT INTO `filed_info` VALUES ('69', '1', '10', 'http://monitoe.com', 'd', '0', '1', '30');
+INSERT INTO `filed_info` VALUES ('70', '1', '10', 'http://monitoe.com', 'd', '0', '1', '30');
 
-LOCK TABLES `filed_info` WRITE;
-/*!40000 ALTER TABLE `filed_info` DISABLE KEYS */;
-/*!40000 ALTER TABLE `filed_info` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `merchant_info`
---
-
+-- ----------------------------
+-- Table structure for `merchant_info`
+-- ----------------------------
 DROP TABLE IF EXISTS `merchant_info`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `merchant_info` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `provice` varchar(45) DEFAULT NULL,
   `city` varchar(45) DEFAULT NULL,
   `town` varchar(45) DEFAULT NULL,
@@ -60,25 +119,58 @@ CREATE TABLE `merchant_info` (
   `merchant_phone` varchar(45) DEFAULT NULL,
   `merchant_field_size` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of merchant_info
+-- ----------------------------
+INSERT INTO `merchant_info` VALUES ('1', '江苏', '南通', '如皋', '商家1', '升名国际', '15861858916', '40');
+INSERT INTO `merchant_info` VALUES ('2', '江苏', '南通', '启东', '商家2', '汇龙', '18817779308', '50');
+INSERT INTO `merchant_info` VALUES ('3', '上海', '浦东', '南汇', '商家3', '陆家嘴', '11122211253', '100');
+INSERT INTO `merchant_info` VALUES ('4', '江苏', '南通', '如皋', '商家1', '升名国际', '15861858916', '40');
+INSERT INTO `merchant_info` VALUES ('5', '江苏', '南通', '启东', '商家2', '汇龙', '18817779308', '50');
+INSERT INTO `merchant_info` VALUES ('6', '上海', '浦东', '南汇', '商家3', '陆家嘴', '11122211253', '100');
+INSERT INTO `merchant_info` VALUES ('7', '江苏', '南通', '如皋', '商家', '升名国际', '15861858917', '45');
+INSERT INTO `merchant_info` VALUES ('8', '江苏', '南通', '如皋', '商家', '升名国际', '15861858917', '45');
+INSERT INTO `merchant_info` VALUES ('9', '江苏', '南通', '如皋', '商家', '升名国际', '15861858917', '45');
+INSERT INTO `merchant_info` VALUES ('10', '江苏', '南通', '如皋', '商家', '升名国际', '15861858917', '45');
+INSERT INTO `merchant_info` VALUES ('11', '江苏', '南通', '如皋', '商家', '升名国际', '15861858917', '45');
+INSERT INTO `merchant_info` VALUES ('12', '江苏', '南通', '如皋', '商家', '升名国际', '15861858917', '45');
+INSERT INTO `merchant_info` VALUES ('13', '江苏', '南通', '如皋', '商家', '升名国际', '15861858917', '45');
+INSERT INTO `merchant_info` VALUES ('14', '江苏', '南通', '如皋', '商家', '升名国际', '15861858917', '45');
+INSERT INTO `merchant_info` VALUES ('15', '江苏', '南通', '如皋', '商家', '升名国际', '15861858917', '45');
+INSERT INTO `merchant_info` VALUES ('16', '江苏', '南通', '如皋', '商家', '升名国际', '15861858917', '45');
+INSERT INTO `merchant_info` VALUES ('17', '江苏', '南通', '如皋', '商家', '升名国际', '15861858917', '45');
+INSERT INTO `merchant_info` VALUES ('18', '江苏', '南通', '如皋', '商家', '升名国际', '15861858917', '45');
+INSERT INTO `merchant_info` VALUES ('19', '江苏', '南通', '如皋', '商家', '升名国际', '15861858917', '45');
+INSERT INTO `merchant_info` VALUES ('20', '江苏', '南通', '如皋', '商家', '升名国际', '15861858917', '45');
+INSERT INTO `merchant_info` VALUES ('21', '江苏', '南通', '如皋', '商家', '升名国际', '15861858917', '45');
+INSERT INTO `merchant_info` VALUES ('22', '江苏', '南通', '如皋', '商家', '升名国际', '15861858917', '45');
+
+-- ----------------------------
+-- Table structure for `user_filed_detail`
+-- ----------------------------
+DROP TABLE IF EXISTS `user_filed_detail`;
+CREATE TABLE `user_filed_detail` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `filed_id` int(11) DEFAULT NULL,
+  `user_phone` varchar(255) DEFAULT NULL,
+  `term` int(11) DEFAULT NULL,
+  `begin_time` datetime DEFAULT NULL,
+  `oever_time` datetime DEFAULT NULL,
+  `price` double DEFAULT NULL,
+  `total` double DEFAULT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `merchant_info`
---
+-- ----------------------------
+-- Records of user_filed_detail
+-- ----------------------------
 
-LOCK TABLES `merchant_info` WRITE;
-/*!40000 ALTER TABLE `merchant_info` DISABLE KEYS */;
-/*!40000 ALTER TABLE `merchant_info` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `user_info`
---
-
+-- ----------------------------
+-- Table structure for `user_info`
+-- ----------------------------
 DROP TABLE IF EXISTS `user_info`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user_info` (
   `user_phone` varchar(11) NOT NULL,
   `nick_name` varchar(45) DEFAULT NULL,
@@ -91,50 +183,27 @@ CREATE TABLE `user_info` (
   `des` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`user_phone`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `user_info`
---
+-- ----------------------------
+-- Records of user_info
+-- ----------------------------
+INSERT INTO `user_info` VALUES ('15861858916', null, null, null, null, '1', '2017-12-26 13:50:09', '2017-12-27 16:28:37', null);
 
-LOCK TABLES `user_info` WRITE;
-/*!40000 ALTER TABLE `user_info` DISABLE KEYS */;
-INSERT INTO `user_info` VALUES ('15861858916',NULL,NULL,NULL,NULL,'1','2017-12-26 13:50:09','2017-12-26 13:50:10',NULL);
-/*!40000 ALTER TABLE `user_info` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `valiate_code`
---
-
+-- ----------------------------
+-- Table structure for `valiate_code`
+-- ----------------------------
 DROP TABLE IF EXISTS `valiate_code`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `valiate_code` (
   `user_phone` varchar(45) NOT NULL,
   `code` varchar(45) NOT NULL,
   `create_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`user_phone`,`code`,`create_time`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `valiate_code`
---
-
-LOCK TABLES `valiate_code` WRITE;
-/*!40000 ALTER TABLE `valiate_code` DISABLE KEYS */;
-INSERT INTO `valiate_code` VALUES ('15861858916','6513','2017-12-26 13:47:10');
-/*!40000 ALTER TABLE `valiate_code` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2017-12-26 15:05:11
+-- ----------------------------
+-- Records of valiate_code
+-- ----------------------------
+INSERT INTO `valiate_code` VALUES ('15861858916', '1141', '2017-12-27 10:56:07');
+INSERT INTO `valiate_code` VALUES ('15861858916', '6513', '2017-12-26 13:47:10');
+INSERT INTO `valiate_code` VALUES ('15861858916', '7873', '2017-12-27 16:27:16');
+INSERT INTO `valiate_code` VALUES ('15861858916', '8711', '2017-12-27 16:27:59');
